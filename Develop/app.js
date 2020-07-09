6bd7556d90453ecdc0f31b07aad2b3b801975543
Joshua Chu <jashya@gmail.com>
2020-07-09 02:27:32 -0400

    Add rendering capability

--- Develop/app.js
+++ Develop/app.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const writeFileAsync = util.promisify(fs.writeFile);
@@ -91,7 +92,8 @@ async function init() {
   await employeeLoop("Manager",1);
   await employeeLoop("Engineer",initInput.engineerCount);
   await employeeLoop("Intern",initInput.internCount);
-  render(employees);
+  await writeFileAsync(outputPath,render(employees));
+  console.log("Done");
 }
 
 async function employeeLoop(role,x) {
